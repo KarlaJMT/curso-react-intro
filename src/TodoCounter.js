e389530810e77552444a393e918fe996a7a7a781
joonchxld <2302091@utrivieramaya.edu.mx>
2026-08-12 21:31:49 -0500

    ToDo Sencillo

---
+++ src/TodoCounter.js
@@ -0,0 +1,8 @@
+function TodoCounter() {
+  return (
+    <h1>Haz completado 3 de 5 ToDo's</h1>
+
+  );
+}
+
+export {TodoCounter};
---
+++ src/TodoSearch.js
@@ -0,0 +1,8 @@
+function TodoSearch() {
+  return (
+    <input placeholder="Comer mango"/>
+
+  );
+}
+
+export { TodoSearch };
